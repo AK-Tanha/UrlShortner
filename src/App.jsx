ba5e6c006ab1a9b15927533de1be33c917f076ae
AK-Tanha <aktanha75@gmail.com>
2026-04-07 06:21:21 +0600

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='text-4xl text-amber-400 font-bold border-2 border-black-500'>
+      Bismillah
+    </div>
+
+  )
+}
+
+export default App
