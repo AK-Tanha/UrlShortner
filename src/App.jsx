687b2762f8295bfb96f26788576da56614f096e8
AK-Tanha <aktanha75@gmail.com>
2026-04-07 13:08:19 +0600

    feat: add Supabase integration and update dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
+import { Button } from './components/ui/button'
+
 
 function App() {
 
   return (
     <div className='text-4xl text-amber-400 font-bold border-2 border-black-500'>
       Bismillah
+
+      <Button variant="default" size="sm">
+        Click me
+      </Button>
+
     </div>
 
   )
